1dac58a402c1e2e3d390a30a5f22c7ba4a00750f
𝐮𝐧𝐬𝐞𝐞𝐧 <darrellcowgill081@gmail.com>
2025-11-18 18:40:46 +0300

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5000,
+    hmr: {
+      clientPort: 443,
+      protocol: 'wss'
+    },
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:3000',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path
+      }
+    }
+  }
+});
